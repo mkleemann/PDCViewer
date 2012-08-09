22ba95767fde51a7b9dc31467202bed1030576d8
Matthias Kleemann <dev@layer128.net>
2012-08-09 21:20:40 +0200

    add missing files to makefiles and project

--- PDCViewer/makedep.mk
+++ PDCViewer/makedep.mk
@@ -4,19 +4,21 @@
 
 bar\bar.c
 
+can\can_error_mcp2515.c
+
 can\can_filter_mcp2515.c
 
 can\can_init_mcp2515.c
 
 can\can_receive_mcp2515.c
 
+can\can_send_mcp2515.c
+
 can\can_sleep_mcp2515.c
 
-can\can_send_mcp2515.c
+PDCViewer.c
 
 spi\spi.c
 
 timer\timer.c
-
-uart\uart.c
 
