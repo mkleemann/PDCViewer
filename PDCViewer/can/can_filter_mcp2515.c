6680abdb7eff44fd5fe4956a3194395cb1c741d7
Matthias Kleemann <dev@layer128.net>
2012-09-29 21:33:41 +0200

    use sequential write to save time and memory for filters and filter masks

--- PDCViewer/can/can_filter_mcp2515.c
+++ PDCViewer/can/can_filter_mcp2515.c
@@ -25,56 +25,43 @@
  * @param chip - select chip to use
  *
  * \note
- * The MCP2515 has to be in configuration mode to set these registers.
+ * The MCP2515 has to be in configuration mode to set these registers. The
+ * function uses sequential write mode to save time and program size.
+ *
  */
 void clear_filters(eChipSelect chip)
 {
+   uint8_t data[12]; // maximum of 12 consecutive registers to write
+   uint8_t i;
+
+   // init filters and mask data
+   for(i = 0; i < 12; ++i)
+   {
+      data[i] = 0;
+   }
+
    // set buffers to receive all messages
    write_register_mcp2515(chip, RXB0CTRL, (1<<RXM1) | (1<<RXM0));
    write_register_mcp2515(chip, RXB1CTRL, (1<<RXM1) | (1<<RXM0));
 
-   // TODO: use possibility to set registers in a row
    // remove all bits from reception filter masks to receive all messages
-   // buffer 0
-   write_register_mcp2515(chip, RXM0SIDH, 0);
-   write_register_mcp2515(chip, RXM0SIDL, 0);
-   write_register_mcp2515(chip, RXM0EID8, 0);
-   write_register_mcp2515(chip, RXM0EID0, 0);
-   // buffer 1
-   write_register_mcp2515(chip, RXM1SIDH, 0);
-   write_register_mcp2515(chip, RXM1SIDL, 0);
-   write_register_mcp2515(chip, RXM1EID8, 0);
-   write_register_mcp2515(chip, RXM1EID0, 0);
-   // filter 0
-   write_register_mcp2515(chip, RXF0SIDH, 0);
-   write_register_mcp2515(chip, RXF0SIDL, 0);
-   write_register_mcp2515(chip, RXF0EID8, 0);
-   write_register_mcp2515(chip, RXF0EID0, 0);
-   // filter 1
-   write_register_mcp2515(chip, RXF1SIDH, 0);
-   write_register_mcp2515(chip, RXF1SIDL, 0);
-   write_register_mcp2515(chip, RXF1EID8, 0);
-   write_register_mcp2515(chip, RXF1EID0, 0);
-   // filter 2
-   write_register_mcp2515(chip, RXF2SIDH, 0);
-   write_register_mcp2515(chip, RXF2SIDL, 0);
-   write_register_mcp2515(chip, RXF2EID8, 0);
-   write_register_mcp2515(chip, RXF2EID0, 0);
-   // filter 3
-   write_register_mcp2515(chip, RXF3SIDH, 0);
-   write_register_mcp2515(chip, RXF3SIDL, 0);
-   write_register_mcp2515(chip, RXF3EID8, 0);
-   write_register_mcp2515(chip, RXF3EID0, 0);
-   // filter 4
-   write_register_mcp2515(chip, RXF4SIDH, 0);
-   write_register_mcp2515(chip, RXF4SIDL, 0);
-   write_register_mcp2515(chip, RXF4EID8, 0);
-   write_register_mcp2515(chip, RXF4EID0, 0);
-   // filter 5
-   write_register_mcp2515(chip, RXF5SIDH, 0);
-   write_register_mcp2515(chip, RXF5SIDL, 0);
-   write_register_mcp2515(chip, RXF5EID8, 0);
-   write_register_mcp2515(chip, RXF5EID0, 0);
+   // mask 0/1
+   write_multi_registers_mcp2515(chip,
+                                 RXM1EID0 - RXM0SIDH + 1,
+                                 RXM0SIDH,
+                                 data);
+
+   // filters 0/1/2
+   write_multi_registers_mcp2515(chip,
+                                 RXF2EID0 - RXF0SIDH + 1,
+                                 RXF0SIDH,
+                                 data);
+
+   // filters 3/4/5
+   write_multi_registers_mcp2515(chip,
+                                 RXF5EID0 - RXF3SIDH + 1,
+                                 RXF3SIDH,
+                                 data);
 }
 
 /**
